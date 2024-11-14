7442d2377c3bb759b620f1f93a5df33e99274006
Yash Unadakt <yash.unadkat@tatvasoft.com>
2024-11-14 17:43:50 +0530

    122 Adding Ingredients to Recipes

--- RecipesWebUI/src/app/recipes/recipe.service.ts
+++ RecipesWebUI/src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://i0.wp.com/picjumbo.com/wp-content/uploads/korean-bibimbap-flatlay.jpg?w=600&quality=80'
+      'Pizza',
+      'A super testy Pizza - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/800px-Pizza-3007395.jpg',
+      [new Ingredient('Pepperoni', 3), new Ingredient('Olive oil', 4)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://i0.wp.com/picjumbo.com/wp-content/uploads/korean-bibimbap-flatlay.jpg?w=600&quality=80'
+      'Burger',
+      'What else you need to say?',
+      'https://www.foodandwine.com/thmb/jldKZBYIoXJWXodRE9ut87K8Mag=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Tomato', 1)]
     ),
   ];
 
